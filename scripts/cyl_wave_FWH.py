0a4012880729fd3f81b0c22999b26ca3658c8603
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2022-09-26 16:51:39 -0300

    novas análises

--- scripts/cyl_wave_FWH.py
+++ scripts/cyl_wave_FWH.py
@@ -3,15 +3,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import hankel2
 from pathlib import Path
-#%% Caminhos
-PATH_DATA   = Path().absolute().parent / 'data'
-print(PATH_DATA)
-PROBES      = PATH_DATA / 'probes' / '0'
-FWH         = PATH_DATA / 'pressureData' 
+#%% FUNÇÕES
+def importData(simulation:str, probe:int=2)-> tuple:
+    PATH_DATA   = Path().absolute().parent / 'data'
+    PROBES      = PATH_DATA / 'probes' 
+    FWH         = PATH_DATA / 'pressureData'
 
+    t, p         = np.loadtxt(PROBES / simulation /'p.txt', 
+                            usecols = (0, probe+1), unpack=True)
+    fwh_t, fwh_p = np.loadtxt(FWH / simulation / 'FWH_surfacePressure.txt', 
+                            usecols=(0,probe+1), unpack=True)
+
+    print(f'Probe: {probe}')
+    return ((t,p), (fwh_t, fwh_p)) 
+
+def plot(FWH:tuple, SIM:tuple, fanalitic:any) -> None:
+    fwh_t, fwh_p    = FWH
+    t, p            = SIM
+   
+    if fanalitic != None:
+        p_2_E           = fanalitic(t) 
+        plt.plot(t, p_2_E, 'k--', label = 'Analítico')
+        plt.plot(t, p - 101325,'r--', label = 'Cálculo Direto', alpha = 0.5)
+    else:
+        plt.plot(t, p - 101325,'r--', label = 'FWH2')
+    plt.plot(fwh_t, fwh_p - 101325, 'b--', label = 'FWH', alpha = 0.35)
+
+    plt.xlabel('Tempo [s]')
+    plt.ylabel('Pressão [Pa]')
+    plt.legend()
+    
+    plt.grid()
+    plt.show()
 #%% Constantes
-rvec = np.arange(2, 102, 10)
-
+rvec = np.arange(2, 112, 10)
+#%% Solução Analítica
 raio = 0.05715 / 2
 S = 0.1
 c0 = 340.29
@@ -20,35 +46,26 @@ freq = 100
 omega = freq * 2 * np.pi
 T0 = 273.15
 T = (c0 / c) ** 2 * T0
-#%% Cálculo da densidade
-rho0 = 101325 / (287.058 * T)
+rho0 = 101325 / (287.058 * T) #% Cálculo da densidade
 area = 2 * np.pi * raio
 velocity = S / area
+
 H_1_fonte_J = hankel2(1, (omega * raio / c0))
-
 A0 = velocity*1j*rho0*c0/H_1_fonte_J
+H_0_E = lambda x: hankel2(0, omega*x/c0)
+P_2_E = lambda x: A0*H_0_E(x)
+p_2_E = lambda t, x=rvec[2]: P_2_E(x)*np.exp(1j*omega*t).imag
 
-#%% Import dados
-probe        = 2
-t, p         = np.loadtxt(PROBES / 'p.txt', 
-                          usecols = (0, probe-1), unpack=True)
-fwh_t, fwh_p = np.loadtxt(FWH / 'FWH_surfacePressure.txt', 
-                           usecols=(0,probe-1), unpack=True)
- 
+#%% Importando dados
+# # SIM, FWH40 = importData('backward40')
+# _,FWH20    = importData('backward20')
+SIM,FWH2     = importData('monopole2')
 
-#%% Analítico
-H_0_E = hankel2(0, omega*rvec/c0)
-P_2_E = A0*H_0_E
-p_2_E = P_2_E*np.exp(1j*omega*t).imag
+#%% Plot
+plot(FWH2, SIM, fanalitic = p_2_E)
 
-# %% Plot
-plt.plot(t, p_2_E[0:len(t)], 'k--', label = 'Analítico')
-plt.plot(t, p - 101325,'r--', label = 'Cálculo Direto')
-plt.plot(fwh_t, fwh_p, 'b--', label = 'FWH')
+# %% testes
+rexp = np.linspace(rvec[0],rvec[-1])
+plt.plot(rexp, p_2_E(t = 0.5,i = rexp))
 
-plt.xlabel('Tempo [s]')
-plt.ylabel('Pressão [Pa]')
-plt.legend()
-
-plt.show()
 # %%

3e6a358a7e313f1ceabb3f25f4c94584e2717089
Widmark Kauê <widmarkkaue.s.c@gmail.com>
2022-09-29 13:14:49 -0300

    função importData pronta (eu acho)

--- scripts/cyl_wave.py
+++ scripts/cyl_wave.py
@@ -1,10 +1,9 @@
 #%% Librarys
-from cProfile import label
 import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
-from scipy.special import hankel2
 from scipy.integrate import trapz
+from scipy.special import hankel2
 
 #%% FUNÇÕES
 def pressure(r:float or np.ndarray = None, 
@@ -26,13 +25,13 @@ def pressure(r:float or np.ndarray = None,
     
     H_0_E       = lambda r1: hankel2(0, omega*r1/c0)
     P_2_E       = lambda r1: A0*H_0_E(r1)
-    p_2_E       = lambda t1,r1: P_2_E(r1)*np.exp(1j*omega*t1).imag
+    p_2_E       = lambda t1,r1: (P_2_E(r1)*np.exp(1j*omega*t1)).imag
     
     if r == None and t == None:
         return p_2_E
     elif any(i != None for i in [t,r]):
         aux     = t == None
-        aux2    = lambda t1: p_2_E(t1,r1=r) if aux else lambda r1: p_2_E(r1, t1=t)
+        aux2    = lambda t1: p_2_E(t1,r1=r) if aux else lambda r1: p_2_E(r=r1, t1=t)
         return aux2
     else:
         return p_2_E(t,r)
@@ -40,6 +39,8 @@ def pressure(r:float or np.ndarray = None,
 
 
 def importData(simulation:str, probe:int=2, time:float = None)-> tuple:
+    
+    assert 0<= probe <= 10, 'ERRO: O valor da probe deve estar entre 0 e 10'
     PATH_DATA   = Path().absolute().parent / 'data'
     PROBES      = PATH_DATA / 'probes' / simulation /'p.txt'
     FWH         = PATH_DATA / 'pressureData' / simulation / 'FWH_surfacePressure.txt'
@@ -52,22 +53,23 @@ def importData(simulation:str, probe:int=2, time:float = None)-> tuple:
         print(f'Probe: {probe}')
         return ((t,p-toPa), (fwh_t, fwh_p-toPa)) 
     else:
-        t   = np.loadtxt(PROBES / simulation /'p.txt', usecols = 0)
-        pos = t.searchsorted(time)
-        p   = np.loadtxt(PROBES, skiprows=pos-1)[1:] 
-        print(f'Time = {t[pos]}')
-        return p-toPa
+        t   = np.loadtxt(PROBES, usecols = 0)
+        pos = np.searchsorted(t,time)-1
+        p   = np.loadtxt(PROBES, skiprows=12 + pos)[1,1:] 
+        print(f'Time = {t[pos]} \nPos = {pos}')
+        return p - toPa
 
 
-def rms(tp:tuple, pfunc:any, r:int, 
-        freq:float = 100, c0:float = 340.29, plot: bool = True):
+def rmsTime(tp:tuple, robs:float, freq:float = 100, 
+            c0:float = 340.29, plot: bool = True)-> float:
         
     
     t, p                = tp
-    transientTime       = r/c0 +5/freq
+    transientTime       = robs/c0 +5/freq
     fil                 = lambda x: x >= transientTime
     efectiveTime        = np.array(list(filter(fil, t)))
-    efectivePressure    = np.array([p[t.index(i)] for i in efectiveTime]) 
+    t0                  = list(t).index(efectiveTime[0])
+    efectivePressure    = p[t0:] 
 
     #% Plot
     if plot:
@@ -80,13 +82,36 @@ def rms(tp:tuple, pfunc:any, r:int,
         plt.show()
     
     #% RMS
-    aux1    = (p - pfunc(efectiveTime))**2
-    rms     = trapz(aux1, efectiveTime)/trapz(pfunc(efectiveTime), efectiveTime)
-    
+    pfunc   = pressure(r = robs)
+    aux1    = (efectivePressure - pfunc(efectiveTime))**2
+    num     = trapz(aux1, efectiveTime)
+    den     = trapz(pfunc(efectiveTime)**2, efectiveTime)
+    rms     = num/den
+
     return rms  
 
+def rmsSpacial(rp:tuple, tobs:float = 0.5, plot:bool = True):
+    
+    r, p    = rp
+    pfunc   = pressure(t = tobs)
+    
+    if plot:
+        plt.plot(r, pfunc(r), 'k-', label = 'Analítico')
+        plt.plot(r, p, 'r-', label = 'Direto')
+        plt.xlabel('raio [m]')
+        plt.ylabel('Pressão [Pa]')
+        plt.legend()
+        plt.grid()
+        plt.show()
+    
+    aux1    = (p - pfunc(r))**2
+    num     = trapz(aux1, r)
+    den     = trapz(pfunc(r)**2, r)
+    rms     = num/den 
 
-def plotTime(FWH:tuple, SIM:tuple, fanalitic:any) -> None:
+    return rms
+
+def plotTime(FWH:tuple, SIM:tuple, fanalitic:any, title:str = None) -> None:
     fwh_t, fwh_p    = FWH
     t, p            = SIM
    
@@ -98,6 +123,7 @@ def plotTime(FWH:tuple, SIM:tuple, fanalitic:any) -> None:
         plt.plot(t, p,'r-.', label = 'FWH2')
     plt.plot(fwh_t, fwh_p, 'b--', label = 'FWH', alpha = 0.35)
 
+    if title!=None: plt.title(title)
     plt.xlabel('Tempo [s]')
     plt.ylabel('Pressão [Pa]')
     plt.legend()
@@ -110,19 +136,19 @@ microphone = np.arange(2, 112, 10)
 #%% Importando dados
 # # SIM, FWH40 = importData('backward40')
 # _,FWH20    = importData('backward20')
-SIM,FWH2     = importData('monopole2')
+SIM,FWH2     = importData('monopole2', probe = 10)
+pr           = importData('monopole2', time  = 0.5)
 
 #%% Plot
-p_time = lambda t: pressure(r = microphone[2], t=t) 
+p_time = pressure(r  = microphone[2])
 plotTime(FWH2, SIM, fanalitic = p_time)
 
-# %% testes
-# rexp = np.linspace(rvec[0],rvec[-1])
-# plt.plot(rexp, p_2_E(t = 0.5,x = rexp), 'k', label = 'analítico')
 
-# plt.xlabel('r [m]')
-# plt.ylabel('Pressão [Pa]')
-# plt.legend()
-# plt.grid()
-# plt.show()
+#%% RMS
+RMS = rmsTime(SIM, r = microphone[2])*100
+print(RMS)
+
+# %% teste
+a = np.loadtxt('teste.txt',skiprows=3)
+print(a)
 # %%

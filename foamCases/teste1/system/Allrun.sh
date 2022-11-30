c10ddf0625b01b058f1382def36ef2ef551eb7be
Widmark Kauê <widmarkkaue.s.c@gmail.com>
2022-11-30 11:30:55 -0300

    OpenFoam case test

---
+++ foamCases/teste1/system/Allrun.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /usr/lib/openfoam/openfoam2112/etc/bashrc
+
+cat $FOAM_RUN
+
+source $WM_PROJECT_DIR/bin/tools/RunFunctions
+
+foamCleanTuturials
+
+restore0Dir
+runApplication gmshToFoam teste.msh
+runApplication changeDictionary
+runApplication decomposePar
+runParallel renumberMesh -overwrite
+runParallel $(getApplication) &

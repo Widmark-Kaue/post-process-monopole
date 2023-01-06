f3f395bf18d3f1586e6d5480cf6da29dfc9bfad4
Widmark Kaue Silva Cardoso <widmark.kaue@grad.ufsc.br>
2023-01-06 16:08:22 -0300

    update

--- src/datadrive.py
+++ src/datadrive.py
@@ -1,8 +1,8 @@
 #%%
 from pathlib import Path
 
-PATH_DATA   = Path('data')
-PATH_WRITE  = Path('observers')
+PATH_DATA   = Path().cwd().with_name('data')
+PATH_WRITE  = Path().cwd().with_name('observers')
 PATH_DATA.mkdir(exist_ok=True)
 PATH_WRITE.mkdir(exist_ok=True)
 
@@ -10,9 +10,9 @@ arq = PATH_DATA.joinpath('teste.txt').absolute()
 print(arq.with_suffix('.dat'))
 arq.replace(arq.with_suffix('.dat'))
 print(arq)
-# for child in PATH_DATA.glob("**/p*"):
+for child in PATH_DATA.glob("**/p*"):
     
-#     if child.is_file():
-#         child.rename('p.dat')
-#         print(child)
+    if child.is_file():
+        child.rename('p.dat')
+        print(child)
  #%%

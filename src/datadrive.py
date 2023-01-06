94e38fb3b68f07aab40cdd6035e68b8e898b4ddf
Widmark Kaue Silva Cardoso <widmark.kaue@grad.ufsc.br>
2023-01-06 13:32:27 -0300

    update

--- src/datadrive.py
+++ src/datadrive.py
@@ -1,9 +1,18 @@
+#%%
 from pathlib import Path
 
-# from google.colab import drive
+PATH_DATA   = Path('data')
+PATH_WRITE  = Path('observers')
+PATH_DATA.mkdir(exist_ok=True)
+PATH_WRITE.mkdir(exist_ok=True)
 
-PATH_DATA = Path('data')
-
-PATH_WRITE = Path('observers')
-if not PATH_WRITE.exists():
-    PATH_WRITE.mkdir()
+arq = PATH_DATA.joinpath('teste.txt').absolute()
+print(arq.with_suffix('.dat'))
+arq.replace(arq.with_suffix('.dat'))
+print(arq)
+# for child in PATH_DATA.glob("**/p*"):
+    
+#     if child.is_file():
+#         child.rename('p.dat')
+#         print(child)
+ #%%

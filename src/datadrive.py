ac35eacfb24b49ca888a29f9f3a497bc95494282
Widmark Kaue Silva Cardoso <widmark.kaue@grad.ufsc.br>
2023-01-09 18:00:34 -0300

    update

--- src/datadrive.py
+++ src/datadrive.py
@@ -1,22 +1,23 @@
 #%%
 from pathlib import Path
 
-PATH_DATA   = Path().cwd().with_name('data')
-PATH_TEST  = Path().cwd().with_name('test')
-PATH_WRITE  = Path().cwd().with_name('observers')
-PATH_DATA.mkdir(exist_ok=True)
-PATH_TEST.mkdir(exist_ok=True)
-PATH_WRITE.mkdir(exist_ok=True)
+PATH_DATA   = Path().cwd().joinpath('data')
+PATH_TEST   = Path().cwd().joinpath('test')
+PATH_WRITE  = Path().cwd().joinpath('observers')
+
+PATH_DATA   .mkdir(exist_ok=True)
+PATH_TEST   .mkdir(exist_ok=True)
+PATH_WRITE  .mkdir(exist_ok=True)
 #%%
 arq =PATH_TEST.joinpath('teste.txt').absolute()
 print(arq.with_suffix('.dat'))
 arq.replace(arq.with_suffix('.dat'))
 print(arq)
 #%%
-for child in PATH_DATA.glob("**/p*"):
+# for child in PATH_DATA.glob("**/p*"):
     
-    if child.is_file():
-        print(child)
-        child.replace(child.with_suffix('.dat'))
-        print(child)
+#     if child.is_file():
+#         print(child)
+#         child.replace(child.with_suffix('.dat'))
+#         print(child)
  #%%
